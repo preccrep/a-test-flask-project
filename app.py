c10989aa0cabfb064214a4962d45292e72933ae3
preccrep <preccrepad@gmail.com>
2021-01-28 02:29:34 +0800

    Add minimal home page

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Welcome to my repository.'
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s' % name
+
+@app.route('/test')
+def url_test_for():
+    print(url_for('hello'))
+    print(url_for('user_page', name='preccrep'))
+    print(url_for('user_page', name='vigohack'))
+    print(url_for('url_test_for'))
+    print(url_for('url_test_for', num=2))
+    return 'test_page'
+
+
