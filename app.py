1d3c1c3fa57a76ee6e7c53554736947463432e80
preccrep <preccrepad@gmail.com>
2021-01-28 03:37:00 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello():
     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 
@@ -17,5 +17,23 @@ def url_test_for():
     print(url_for('url_test_for'))
     print(url_for('url_test_for', num=2))
     return 'test_page'
+
+name = 'preccrep'
+movies = [
+{'title': 'My Neighbor Totoro', 'year': '1988'},
+{'title': 'Dead Poets Society', 'year': '1989'},
+{'title': 'A Perfect World', 'year': '1993'},
+{'title': 'Leon', 'year': '1994'},
+{'title': 'Mahjong', 'year': '1996'},
+{'title': 'Swallowtail Butterfly', 'year': '1996'},
+{'title': 'King of Comedy', 'year': '1999'},
+{'title': 'Devils on the Doorstep', 'year': '1999'},
+{'title': 'WALL-E', 'year': '2008'},
+{'title': 'The Pork of Music', 'year': '2012'},
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 
